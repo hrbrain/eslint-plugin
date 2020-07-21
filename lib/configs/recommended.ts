9c62f1e1abcaf6670f211c8df2080798491c2411
hrb-yamamoto-katsuya <67670052+hrb-yamamoto-katsuya@users.noreply.github.com>
2020-07-21 18:40:22 +0900

    feat: fix add rule to config (#5)

--- lib/configs/recommended.ts
+++ lib/configs/recommended.ts
@@ -2,5 +2,6 @@ export = {
   extends: require.resolve("./base"),
   rules: {
     "@hrbrain/use-name-space-import": "warn",
+    "@hrbrain/no-use-function-component-type-without-children": "warn",
   },
 };
